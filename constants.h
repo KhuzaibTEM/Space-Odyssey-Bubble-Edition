67d1ed34afe88c99eaf85100bcd92861129eaa6d
Khuzaib <79333922+KhuzaibTEM@users.noreply.github.com>
2025-12-02 03:42:09 +0500

    Add button dimensions and font size constants

--- constants.h
+++ constants.h
@@ -8,6 +8,16 @@ const int MAX_COLS = 20;
 
 const float pi = 3.14159f;
 
+const int GameFontSize = 20;
+
+const int ButtonX = 500;
+const int ButtonPlayY = 200; 
+const int ButtonSaveY = 320; 
+const int ButtonExitY = 430;
+
+const int buttonWidth = 300;
+const int buttonHeight = 80;
+
 const float desiredBallDiameter = 20.f;
 const float originalFrameSize = 64.f;   // each bubble tile is 64x64
 const float ballScaleFactor = desiredBallDiameter / originalFrameSize;

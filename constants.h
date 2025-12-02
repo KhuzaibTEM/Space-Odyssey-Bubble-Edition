98826581939a8d7a16d541894d57e4d7fa106a67
Khuzaib <79333922+KhuzaibTEM@users.noreply.github.com>
2025-12-03 04:23:44 +0500

    Add new button constants

--- constants.h
+++ constants.h
@@ -12,11 +12,17 @@ const int GameFontSize = 20;
 
 const int ButtonX = 500;
 const int ButtonPlayY = 200; 
-const int ButtonSaveY = 320; 
-const int ButtonExitY = 430;
+const int ButtonSettingY = 320; 
+const int ButtonSaveY = 430; 
+const int ButtonExitY = 540;
 
 const int buttonWidth = 300;
 const int buttonHeight = 80;
+
+const float WIDTH_BUTTON_IN_G_MENU = 40.f;
+const float HEIGHT_BUTTON_IN_G_MENU = 40.f;
+const float X_BUTTON_IN_G_MENU = WIDTH - 60.f;
+const float Y_BUTTON_IN_G_MENU = HEIGHT - 60.f;
 
 const float desiredBallDiameter = 20.f;
 const float originalFrameSize = 64.f;   // each bubble tile is 64x64

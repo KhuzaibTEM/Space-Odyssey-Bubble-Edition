eb1aeff30a7127ebb45fd912c2327c2342f6e514
Khuzaib <79333922+KhuzaibTEM@users.noreply.github.com>
2025-12-07 18:20:54 +0500

    Add new constants for audio and button dimensions
    Added default music and SFX volume constants, along with pause button dimensions and back button coordinates.

--- constants.h
+++ constants.h
@@ -1,6 +1,7 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
+
 const int WIDTH = 1280;
 const int HEIGHT = 720;
 const int MAX_ROWS = 20;
@@ -10,11 +11,22 @@ const float pi = 3.14159f;
 
 const int GameFontSize = 20;
 
+const float defaultMusicVolume = 30.f;
+const float defaultSfxVolume = 80.f;
+
 const int ButtonX = 500;
 const int ButtonPlayY = 200; 
 const int ButtonSettingY = 320; 
 const int ButtonSaveY = 430; 
 const int ButtonExitY = 540;
+
+const int pauseX = WIDTH - 60;
+const int pauseY = HEIGHT - 60;
+const int pauseW = 40;
+const int pauseH = 40;
+
+const int ButtonBackX = 370;
+const int ButtonBackY = 70;
 
 const int buttonWidth = 300;
 const int buttonHeight = 80;
@@ -23,6 +35,10 @@ const float WIDTH_BUTTON_IN_G_MENU = 40.f;
 const float HEIGHT_BUTTON_IN_G_MENU = 40.f;
 const float X_BUTTON_IN_G_MENU = WIDTH - 60.f;
 const float Y_BUTTON_IN_G_MENU = HEIGHT - 60.f;
+
+const float settingsToggleRadius = 18.f;
+
+const float settingsClickCooldown = 0.25f;
 
 const float desiredBallDiameter = 20.f;
 const float originalFrameSize = 64.f;   // each bubble tile is 64x64

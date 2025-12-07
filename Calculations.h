6cade1105f03e685ef310d63c5631a8749bbd3d5
Khuzaib <79333922+KhuzaibTEM@users.noreply.github.com>
2025-12-07 21:55:07 +0500

    Add IntegerToString function and renamed the header file
    Added IntegerToString function for converting integers to strings.
    Renamed AngleCal to Calculations

--- Calculations.h
+++ Calculations.h
@@ -10,6 +10,7 @@ float FindAngle(sf::Vector2i mousePos, sf::Vector2f spritePos);
 float RadianToDegree(float Number);
 float DegreeToRadian(float Number);
 float rounding(float Number);
+std::string IntegerToString(int num);
 
 float FindAngle(sf::Vector2i mousePos, sf::Vector2f spritePos) {
     // Calculate the angle between the sprite and the mouse cursor
@@ -35,6 +36,25 @@ float DegreeToRadian(float Num) {
 float rounding(float num) {
     if (num - static_cast<int>(num) >= 0.5) return static_cast<int>(num) + 1;
     else return static_cast<int>(num);
+}
+
+std::string IntegerToString(int num) {
+    std::string ans = "";
+    int size10 = 1;
+    int temp = num;
+    int size = 0;
+    while (temp > 0) {
+        size10 *= 10;
+        temp /= 10;
+        size++;
+    }
+    size10 /= 10;
+    for (int i = 0; i < size; i++) {
+        ans += static_cast<char>(num/size10 + 48);
+        num %= size10;
+        size10 /= 10;
+    }
+    return ans;
 }
 
 #endif

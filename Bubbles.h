7910b7fae99cdcdedcfbf690321e9ecfc2cf9a7b
Khuzaib <79333922+KhuzaibTEM@users.noreply.github.com>
2025-12-11 00:51:03 +0500

    Removed unused function (initializeBubbles)

--- Bubbles.h
+++ Bubbles.h
@@ -5,29 +5,7 @@
 #include <SFML/Graphics.hpp>
 #include "constants.h"
 
-void initializeBubbles(sf::Sprite bubbles[][20], sf::Texture& texture);
 void GridMaker(sf::Sprite TopBubbleGridInit[][20], bool BubblesOccupiedOnTop[][20], sf::Sprite BubbleSpriteTop);
-
-// Function to initialize bubbles using arrays
-void initializeBubbles(sf::Sprite bubbles[3][20], sf::Texture& texture) {
-    const int bubbleSize = 64; // Size of each bubble in the texture
-
-    // Define the positions for the bubbles
-    for (int row = 0; row < 3; row++) {
-        for (int col = 0; col < 20; col++) {
-            sf::Sprite bubble(texture);
-
-            // Randomize the color of the bubble
-            int RandomColorSelecter = rand() % 4; // 4 colors in the texture
-            bubble.setTextureRect(sf::IntRect(RandomColorSelecter * bubbleSize, 0, bubbleSize, bubbleSize));
-
-            // Set the position of the bubble
-            bubble.setPosition(col * bubbleSize, row * bubbleSize);
-
-            bubbles[row][col] = bubble;
-        }
-    }
-}
 
 void GridMaker(sf::Sprite bubbleGrid[][20], bool occupied[][20], sf::Sprite BubbleSpriteTop) {
 
